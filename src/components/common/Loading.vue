4b3e7da54a85a4256ec02d446f818ad009739e00
魏子涵 <muzhenweizh@gmail.com>
2023-02-27 16:12:57 +0800

    feat: 🎸 loading component

---
+++ src/components/common/Loading.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="fixed top-0 bottom-0 left-0 right-0 bg-white/70 flex items-center justify-center">
+    <i-mdi:loading class="animate-spin text-[10rem]" />
+  </div>
+</template>
--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,60 +1,39 @@
-import { defineStore } from 'pinia'
-import { useAsyncState } from '@vueuse/core'
-// import localResumeData from '@/resume/resume.test.json'
-import localResumeData from '@/resume/resume.local.json'
+import config from '@/config/jasume.config.json'
 
 export const useStore = defineStore('main', () => {
-  // state
-  const [
-    gistUsername,
-    gistId,
-  ] = ['MoozonWei', '70c30e0182fdf013aa6454f10c9db299']
-  // const remoteResumeData = await getResumeData(gistUsername, gistId)
+  // get resume data from gist
   const {
     state: gistState,
     isReady: gistIsReady,
     isLoading: gistIsLoading,
   } = useAsyncState(
-    getGistResumeData(gistUsername, gistId),
+    getGistResumeData(config?.gist?.username, config?.gist?.id),
     {},
   )
-
+  // get resume data from local public/resume.json file
   const {
     state: localState,
     isReady: localIsReady,
     isLoading: localIsLoading,
   } = useAsyncState(
     getLocalResumeData(),
-    {},
+    defaultResumeData,
   )
 
   const resumeData = computed(
     () => (
       gistIsReady.value
         ? gistState.value
-        : localIsReady.value
-          ? localState.value
-          : localResumeData
+        : localState.value
     ),
   )
-  // get languages from resume.json
   const lang = 'en'
-  const langs = computed(() => _.keys(resumeData.value))
-  const defaultOrder = [
-    'about',
-    'projects',
-    'publications',
-    'work',
-    'skills',
-    'education',
-    'awards',
-    'languages',
-    'certificates',
-    'volunteer',
-    'interests',
-    'references',
-  ]
-  const order: string[] = resumeData.value.order as string[] || defaultOrder
+  const langs = computed(
+    () => _.keys(resumeData.value),
+  )
+  const order = computed(
+    () => resumeData.value.order as string[] || defaultSectionOrder,
+  )
 
   return {
     lang,
