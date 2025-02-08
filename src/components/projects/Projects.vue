4b15aa522787cf25d49a014bae7114efb290aaaa
Moozon <muzhenweizh@gmail.com>
2025-02-08 22:18:08 +0800

    chore: update dep

--- src/components/projects/Projects.vue
+++ src/components/projects/Projects.vue
@@ -4,16 +4,9 @@ const projects = getSectionComputedData('projects')
 </script>
 
 <template>
-  <SectionBox
-    v-if="projects && projects.length > 0"
-    :title="t('title.projects')"
-  >
+  <SectionBox v-if="projects && projects.length > 0" :title="t('title.projects')">
     <div class="projects-container">
-      <ProjectCard
-        v-for="project in projects"
-        :key="project.name"
-        :project="project"
-      />
+      <ProjectCard v-for="project in projects" :key="project.name" :project="project" />
     </div>
   </SectionBox>
 </template>
